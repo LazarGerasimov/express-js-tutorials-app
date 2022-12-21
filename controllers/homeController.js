a9769df3a55f04efa9ce4cdd389d33c6495e003a
Laz <lazgerasimov@gmail.com>
2022-12-21 05:44:22 +0000

    add guest/user home scaffs

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -2,10 +2,19 @@ const homeController = require('express').Router();
 
 //TODO replace with assignment
 homeController.get('/', (req, res) => {
-    res.render('home', {
-        title: 'Home',
-        user: req.user
-    });
+
+    if (req.user) {
+        res.render('user-home', {
+            title: 'User-Home',
+            user: req.user
+        });
+    } else {
+        res.render('guest-home', {
+            title: 'Guest Home',
+            
+        })
+    }
+    
 })
 
 module.exports = homeController;
